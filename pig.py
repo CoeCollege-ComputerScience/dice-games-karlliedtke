36ae304c60b5d450ca9ee61e7ecc04c59fe4c0a2
karlliedtke <kpliedtke24@coe.edu>
2025-03-14 01:27:45 +0000

    pig done

--- pig.py
+++ pig.py
@@ -1,45 +1,54 @@
 import random
+
 def diceroll():
-    dice = random.randint(1,6)
+    dice = random.randint(1, 6)
     return dice
-result = diceroll()
 
 def pig():
     total_player_1 = 0
     total_player_2 = 0
     player = 1
 
-    while total_player_1 < 20 and total_player_2 < 20:
+    while True:
         turn_total = 0
-        print (f"\nplayer {player}'s turn")
+        print(f"\nplayer {player}'s turn")
+     
         roll = diceroll()
 
         while roll != 1:
-            print(f"player {player} rolled a {roll}")
+            print(f"\nplayer {player} rolled a {roll}")
             turn_total += roll
             print(f"turn total: {turn_total}")
+            if player == 1:
+                print(f"player 1 total before current turn: {total_player_1}\n")
+            elif player == 2:
+                print(f"player 2 total before current turn: {total_player_2}\n")
+            if turn_total >= 20:
+                print(f"player {player} wins with a turn total of {turn_total}")
+                return
             choice = input("do you want to hold or roll? hold/roll: ").strip().lower()
             if choice == "hold":
                 break
             roll = diceroll()
 
         if roll == 1:
-            print(f"player {player} rolled a 1, no points for this turn")
-            turn_total = 0      
+            print(f"\nplayer {player} rolled a 1, no points for this turn")
+            turn_total = 0
 
         if player == 1:
             total_player_1 += turn_total
-            print(f"player 1 total: {total_player_1}")
+            print(f"\nplayer 1 total: {total_player_1}")
+            if total_player_1 >= 20:
+                print("player 1 wins")
+                return
             player = 2
         else:
             total_player_2 += turn_total
-            print(f"player 2 total: {total_player_2}")
+            print(f"\nplayer 2 total: {total_player_2}")
+            if total_player_2 >= 20:
+                print("player 2 wins")
+                return
             player = 1
-
-        if total_player_1 >= 20:
-            print("player 1 wins")
-        else:
-            print("player 2 wins")  
 
 pig()
        
